8550a223ae30b00d4f7b4f168eaa08010df086cd
randomap1 <118092966+randomap1@users.noreply.github.com>
2022-11-13 11:59:39 +0700

    Create 300v250.js

---
+++ 300v250.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : 'dad08d7aa3448a51bf7bc0901ede9fa8',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/dad08d7aa3448a51bf7bc0901ede9fa8/invoke.js"></scr' + 'ipt>');
