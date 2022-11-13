87a7d51d5d19a829654c5fbe2a6562a7b9cc826d
randomap1 <118092966+randomap1@users.noreply.github.com>
2022-11-13 12:28:20 +0700

    Create 728v90.js

---
+++ 728v90.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '51639f4fadf1d9fcc35f8e8a7e6e1832',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://capricedes.com/51639f4fadf1d9fcc35f8e8a7e6e1832/invoke.js"></scr' + 'ipt>');
